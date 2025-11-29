f410cf5daf3b5aa70073bf642691e6618a3ef1f8
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-11-29 23:18:13 +0300

    Верстка лэйаута

---
+++ app/components/LayoutLeft.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="layout__block">
+		<NuxtLink to="/">
+			<Icon
+				name="icons:home"
+				size="24px"
+			/>
+		</NuxtLink>
+		<NuxtLink to="/error404">
+			<Icon
+				name="icons:exit"
+				size="24px"
+			/>
+		</NuxtLink>
+	</div>
+</template>
+
+<style scoped>
+.layout__block {
+	height: 100vh;
+	padding: 42px 28px 45px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	border-right: 1px solid var(--color-gray);
+
+	a {
+		text-decoration: none;
+		color: var(--color-black);
+
+		&:hover {
+			color: var(--color-black-soft);
+		}
+	}
+}
+</style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="layout__wrrapper">
+		<LayoutLeft />
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+.layout__wrrapper {
+	display: flex;
+}
+</style>
