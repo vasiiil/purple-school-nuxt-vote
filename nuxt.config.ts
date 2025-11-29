773128dd3ad53b2e13818c36e1a2dcd9dfa8eb27
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-11-29 23:17:35 +0300

    Иконки

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	icon: {
+		customCollections: [
+			{
+				prefix: 'icons',
+				dir: './app/assets/icons',
+			},
+		],
+	},
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/scripts'
-  ]
-})
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/fonts',
+		'@nuxt/image',
+		'@nuxt/scripts',
+		'@nuxt/icon',
+	],
+});
