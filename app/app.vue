f2fd074692f87ecd329c5c7f5a537229baa398e8
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-12-01 23:55:47 +0300

    Анимация переходов

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,29 @@
 <template>
 	<NuxtLayout>
-		<NuxtPage />
+		<main class="main-content">
+			<NuxtPage />
+		</main>
 	</NuxtLayout>
 </template>
 
 <script setup lang="ts">
 import '~/assets/styles/main.css';
 </script>
+
+<style scoped>
+.main-content {
+	width: 690px;
+	margin-top: 40px;
+	margin-left: 215px;
+}
+.page-transition {
+	&-enter-active,
+	&-leave-active {
+		transition: all 0.2s;
+	}
+	&-enter-from,
+	&-leave-to {
+		opacity: 0;
+	}
+}
+</style>
