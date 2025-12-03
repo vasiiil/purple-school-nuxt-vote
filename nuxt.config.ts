d20d128605e2f3f4667f40897bbaca6842530761
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-12-03 22:59:21 +0300

    Добавил API url в конфиг

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
+	runtimeConfig: {
+		token: '',
+		public: {
+			apiurl: '',
+		},
+	},
 	icon: {
 		customCollections: [
 			{
