ff99df2911838ab0eae85eefa23cdfc799efc30e
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-12-03 22:59:55 +0300

    Интерфейсы постов

---
+++ app/interfaces/post.interface.ts
@@ -0,0 +1,19 @@
+export interface IPost {
+	id: number;
+	title: string;
+	content: string;
+	author_id: number;
+	likes: number;
+	dislikes: number;
+	rating: number;
+	published_at: string;
+	updated_at: string;
+}
+
+export interface IGetPostsResponse {
+	posts: IPost[] | null;
+	total: number;
+	page: number;
+	page_size: number;
+	total_pages: number;
+}
