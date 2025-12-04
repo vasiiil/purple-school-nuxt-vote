b906a176a4a5960ccbd14e0eb1d3b7e97a21c6b5
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-12-05 00:16:52 +0300

    Стор лайков/дизлайков

---
+++ app/stores/votes.store.ts
@@ -0,0 +1,34 @@
+import type { IPost } from '~/interfaces/post.interface';
+
+export const useVotesStore = defineStore(
+	'votes',
+	() => {
+		const config = useRuntimeConfig();
+		const apiUrl = config.public.apiurl;
+
+		const votes = ref<Record<number, 'like' | 'dislike'>>({});
+		async function like(id: number) {
+			votes.value[id] = 'like';
+			return await $fetch<IPost>(`${apiUrl}/posts/${id}/like`, { method: 'POST' });
+		}
+		async function dislike(id: number) {
+			votes.value[id] = 'dislike';
+			return await $fetch<IPost>(`${apiUrl}/posts/${id}/dislike`, { method: 'POST' });
+		}
+		function isLike(id: number) {
+			return votes.value[id] === 'like';
+		}
+		function isDislike(id: number) {
+			return votes.value[id] === 'dislike';
+		}
+
+		return {
+			votes,
+			like,
+			dislike,
+			isLike,
+			isDislike,
+		};
+	},
+	{ persist: true }
+);
